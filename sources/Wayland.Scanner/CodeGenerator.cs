a61bd956d51ffa94b15d82e52b89d70ae3ffd764
ktlwap <149824361+ktlwap@users.noreply.github.com>
2024-03-26 21:35:16 +0100

    Implement wayland scanner

--- sources/Wayland.Scanner/CodeGenerator.cs
+++ sources/Wayland.Scanner/CodeGenerator.cs
@@ -10,38 +10,39 @@ public static class CodeGenerator
 
     public static void Generate(Protocol protocol)
     {
-        string[] filenames = Directory.GetFiles(ServerFilePath);
+        var filenames = Directory.GetFiles(ServerFilePath);
         if (!filenames.Any(f =>
                 f.EndsWith("Wayland.Protocol.Server.csproj") || f.EndsWith("Wayland.Protocol.Client.csproj")))
             throw new Exception("Output directory is not correct. Please set it to the correct path.");
 
 
-        foreach (string filename in filenames)
-        {
+        foreach (var filename in filenames)
             if (filename.EndsWith(".gen.cs"))
                 File.Delete(filename);
-        }
 
-        foreach (Interface @interface in protocol.Interfaces)
+        foreach (var @interface in protocol.Interfaces)
         {
             if (@interface.Name is "Shm" or "DataOffer")
                 // These are not supported yet due to fd as argument.
                 continue;
 
-            using FileStream fileStream = File.Create(Path.Combine(ServerFilePath, @interface.Name + ".gen.cs"));
+            using var fileStream = File.Create(Path.Combine(ServerFilePath, @interface.Name + ".gen.cs"));
 
-            byte[] source = GenerateSource(@interface);
+            var source = GenerateSource(@interface);
             fileStream.Write(source, 0, source.Length);
         }
     }
 
     private static byte[] GenerateSource(Interface @interface)
     {
-        StringBuilder sb = new StringBuilder();
-        sb.Append("namespace Wayland.Protocol.Common\n");
+        var sb = new StringBuilder();
+        sb.Append("namespace Wayland.Protocol.Common;\n");
         sb.Append('\n');
         sb.Append($"public sealed class {@interface.Name} : ProtocolObject\n");
         sb.Append("{\n");
+        sb.Append("    public readonly EventsWrapper Events;\n");
+        sb.Append("    public readonly RequestsWrapper Requests;\n");
+        sb.Append('\n');
         sb.Append($"    public {@interface.Name}(uint id, uint version) : base(id, version)\n");
         sb.Append("    {\n");
         sb.Append("        Events = new EventsWrapper(this);\n");
@@ -62,24 +63,18 @@ public static class CodeGenerator
 
     private static void AddEventOpCodeStub(StringBuilder sb, List<Event> events)
     {
-        sb.Append("    public enum RequestOpCode : ushort\n");
+        sb.Append("    private enum EventOpCode : ushort\n");
         sb.Append("    {\n");
-        for (int i = 0; i < events.Count; i++)
-        {
-            sb.Append($"        {events[i].Name} = {i},\n");
-        }
+        for (var i = 0; i < events.Count; i++) sb.Append($"        {events[i].Name} = {i},\n");
 
         sb.Append("    }\n");
     }
 
     private static void AddRequestOpCodeStub(StringBuilder sb, List<Request> requests)
     {
-        sb.Append("    public enum RequestOpCode : ushort\n");
+        sb.Append("    private enum RequestOpCode : ushort\n");
         sb.Append("    {\n");
-        for (int i = 0; i < requests.Count; i++)
-        {
-            sb.Append($"        {requests[i].Name} = {i},\n");
-        }
+        for (var i = 0; i < requests.Count; i++) sb.Append($"        {requests[i].Name} = {i},\n");
 
         sb.Append("    }\n");
     }
@@ -88,11 +83,13 @@ public static class CodeGenerator
     {
         sb.Append("    public class EventsWrapper(ProtocolObject protocolObject)\n");
         sb.Append("    {\n");
-        foreach (Event @event in events)
-        {
-            sb.Append(
-                $"        public Action<{string.Join(", ", @event.Arguments.Select(arg => arg.Type))}>? {@event.Name} {{ get; set; }}\n");
-        }
+        foreach (var @event in events)
+            if (@event.Arguments.Count > 0)
+                sb.Append(
+                    $"        public Action<{string.Join(", ", @event.Arguments.Select(arg => arg.Type))}>? {@event.Name} {{ get; set; }}\n");
+            else
+                sb.Append(
+                    $"        public Action? {@event.Name} {{ get; set; }}\n");
 
         sb.Append("        \n");
         sb.Append("        internal void HandleEvent(SocketConnection socketConnection)\n");
@@ -103,7 +100,7 @@ public static class CodeGenerator
         sb.Append("            switch (opCode)\n");
         sb.Append("            {\n");
 
-        foreach (Event @event in events)
+        foreach (var @event in events)
         {
             sb.Append($"                case (ushort) EventOpCode.{@event.Name}:\n");
             sb.Append($"                    Handle{@event.Name}Event(socketConnection, length);\n");
@@ -114,11 +111,54 @@ public static class CodeGenerator
         sb.Append("        }\n");
         sb.Append("        \n");
 
-        foreach (Event @event in events)
+        foreach (var @event in events)
         {
             sb.Append(
                 $"        private void Handle{@event.Name}Event(SocketConnection socketConnection, ushort length)\n");
             sb.Append("        {\n");
+            sb.Append("            byte[] buffer = new byte[length / 8];\n");
+            sb.Append("            socketConnection.Read(buffer, 0, buffer.Length);\n");
+            sb.Append('\n');
+            sb.Append("            MessageReader reader = new MessageReader(buffer);\n");
+            sb.Append('\n');
+
+            for (var i = 0; i < @event.Arguments.Count; ++i)
+            {
+                var argument = @event.Arguments[i];
+                switch (argument.Type)
+                {
+                    case "int":
+                        sb.Append($"            int arg{i} = reader.ReadInt();\n");
+                        break;
+                    case "uint":
+                        sb.Append($"            uint arg{i} = reader.ReadUInt();\n");
+                        break;
+                    case "Fixed":
+                        sb.Append($"            Fixed arg{i} = reader.ReadFixed();\n");
+                        break;
+                    case "NewId":
+                        sb.Append($"            NewId arg{i} = reader.ReadNewId();\n");
+                        break;
+                    case "ObjectId":
+                        sb.Append($"            ObjectId arg{i} = reader.ReadObjectId();\n");
+                        break;
+                    case "string":
+                        sb.Append($"            string arg{i} = reader.ReadString();\n");
+                        break;
+                    case "Fd":
+                        sb.Append($"            Fd arg{i} = reader.ReadFd();\n");
+                        break;
+                    case "byte[]":
+                        sb.Append($"            byte[] arg{i} = reader.ReadByteArray();\n");
+                        break;
+                    default:
+                        throw new Exception("Unknown type: " + argument.Type);
+                }
+            }
+
+            sb.Append('\n');
+            sb.Append(
+                $"            {@event.Name}?.Invoke({string.Join(", ", @event.Arguments.Select((_, i) => "arg" + i))});\n");
             sb.Append("        }\n");
             sb.Append("        \n");
         }
@@ -130,7 +170,7 @@ public static class CodeGenerator
     {
         sb.Append("    public class RequestsWrapper(ProtocolObject protocolObject)\n");
         sb.Append("    {\n");
-        foreach (Request request in requests)
+        foreach (var request in requests)
         {
             if (request.Arguments.Count > 0)
                 sb.Append(
@@ -139,39 +179,26 @@ public static class CodeGenerator
                 sb.Append($"        public void {request.Name}(SocketConnection socketConnection)\n");
 
             sb.Append("        {\n");
-            sb.Append("            byte[] buffer = new byte[length / 8];\n");
-            sb.Append("            socketConnection.Read(buffer, 0, buffer.Length);\n");
-            sb.Append('\n');
-            sb.Append("            int index = 0;\n");
+            sb.Append("            MessageWriter writer = new MessageWriter();\n");
+            sb.Append("            writer.Write(protocolObject.Id);\n");
+            sb.Append($"            writer.Write((int) RequestOpCode.{request.Name});\n");
 
-            for (int i = 0; i < request.Arguments.Count; ++i)
+            for (var i = 0; i < request.Arguments.Count; ++i)
             {
-                Argument argument = request.Arguments[i];
+                var argument = request.Arguments[i];
                 switch (argument.Type)
                 {
                     case "int":
-                        sb.Append(
-                            $"            int arg{i} = (int)(buffer[index++] << 8 & buffer[index++]);\n");
-                        break;
                     case "uint":
-                        sb.Append(
-                            $"            uint arg{i} = (uint)(buffer[index++] << 8 & buffer[index++]);\n");
-                        break;
-                    case "Fixed":
-                        sb.Append(
-                            $"            uint arg{i} = new Fixed((int)buffer[index++] << 8 & buffer[index++]);\n");
-                        break;
-                    case "NewId":
-                        sb.Append(
-                            $"            uint arg{i} = new NewId((uint)buffer[index++] << 8 & buffer[index++]);\n");
-                        break;
-                    case "ObjectId":
-                        sb.Append(
-                            $"            uint arg{i} = new ObjectId((uint)buffer[index++] << 8 & buffer[index++]);\n");
-                        break;
                     case "string":
                         sb.Append(
-                            $"            uint arg{i} = StringConverter.Parse(buffer, in index, out index);\n");
+                            $"            writer.Write({argument.Name});\n");
+                        break;
+                    case "Fixed":
+                    case "ObjectId":
+                    case "NewId":
+                        sb.Append(
+                            $"            writer.Write({argument.Name}.Value);\n");
                         break;
                     default:
                         throw new Exception("Unknown type: " + argument.Type);
@@ -179,8 +206,12 @@ public static class CodeGenerator
             }
 
             sb.Append('\n');
-            sb.Append(
-                $"            {request.Name}?.Invoke({string.Join(", ", request.Arguments.Select(arg => arg.Name))});\n");
+            sb.Append("            byte[] data = writer.ToArray();\n");
+            sb.Append("            int length = data.Length - 8;\n");
+            sb.Append("            data[5] = (byte)(length >> 8);\n");
+            sb.Append("            data[6] = (byte)(byte.MaxValue << 8 & length);\n");
+            sb.Append('\n');
+            sb.Append("            socketConnection.Write(data);\n");
             sb.Append("        }\n");
             sb.Append('\n');
         }
--- sources/Wayland.Scanner/Symbolizer.cs
+++ sources/Wayland.Scanner/Symbolizer.cs
@@ -1,10 +1,18 @@
 using Wayland.Scanner.Types.Xml;
-using Enum = Wayland.Scanner.Types.Xml.Enum;
+using Enum = System.Enum;
 
 namespace Wayland.Scanner;
 
 public static class Symbolizer
 {
+    private static readonly string[] ReservedKeywords =
+    [
+        "event",
+        "class",
+        "object",
+        "interface"
+    ];
+
     public static List<Interface> Process(List<Interface> interfaces)
     {
         foreach (Interface @interface in interfaces)
@@ -14,76 +22,77 @@ public static class Symbolizer
             foreach (Event @event in @interface.Events)
             {
                 @event.Name = ProcessSymbol(@event.Name, true);
-                foreach (Argument @argument in @event.Arguments)
+                foreach (Argument argument in @event.Arguments)
                 {
-                    @argument.Name = ProcessSymbol(@argument.Name, false);
-                    @argument.Type = ProcessType(@argument.Type);
+                    argument.Name = ProcessSymbol(argument.Name, false);
+                    argument.Type = ProcessType(argument.Type);
                 }
             }
-            
+
             foreach (Request request in @interface.Requests)
             {
                 request.Name = ProcessSymbol(request.Name, true);
-                foreach (Argument @argument in request.Arguments)
+                foreach (Argument argument in request.Arguments)
                 {
-                    @argument.Name = ProcessSymbol(@argument.Name, false);
-                    @argument.Type = ProcessType(@argument.Type);
+                    argument.Name = ProcessSymbol(argument.Name, false);
+                    argument.Type = ProcessType(argument.Type);
                 }
             }
-            
-            foreach (Enum @enum in @interface.Enums)
-            {
+
+            foreach (Types.Xml.Enum @enum in @interface.Enums)
                 @enum.Name = ProcessSymbol(@enum.Name, true);
-            }
         }
 
         return interfaces;
     }
-    
+
     private static string ProcessSymbol(string symbol, bool capitalizeFirstLetter)
     {
         if (symbol.StartsWith("wl_"))
             symbol = symbol.Remove(0, 3);
-        
+
         if (capitalizeFirstLetter)
             symbol = char.ToUpper(symbol[0]) + symbol[1..];
-        
+
         if (symbol.Contains('_'))
         {
             List<char> chars = symbol.ToCharArray().ToList();
-            
+
             int i = 0;
             while (i < chars.Count)
             {
                 if (chars[i] == '_')
                 {
                     chars.RemoveAt(i);
-                    
+
                     if (i < chars.Count)
                         chars[i] = char.ToUpper(chars[i]);
                 }
 
                 ++i;
             }
-            
+
             symbol = new string(chars.ToArray());
         }
-        
+
+        if (ReservedKeywords.Contains(symbol))
+            symbol = '@' + symbol;
+
         return symbol;
     }
-    
+
     private static string ProcessType(string type)
     {
         return type switch
         {
+            "array" => "byte[]",
             "int" => "int",
             "uint" => "uint",
             "fixed" => "Fixed",
-            "object" => "ObjectId",
-            "new_id" => "NewId",
             "string" => "string",
             "fd" => "Fd",
-            "array" => "void*",
+            "object" => "ObjectId",
+            "new_id" => "NewId",
             _ => throw new NotSupportedException($"Type '{type}' is not supported.")
         };
     }
