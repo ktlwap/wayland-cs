783b2fa57b480d975f50265d713ad69448e9ffed
ktlwap <149824361+ktlwap@users.noreply.github.com>
2024-03-26 21:59:00 +0100

    Extend scanner

--- sources/Wayland.Scanner/CodeGenerator.cs
+++ sources/Wayland.Scanner/CodeGenerator.cs
@@ -10,30 +10,57 @@ public static class CodeGenerator
 
     public static void Generate(Protocol protocol)
     {
-        var filenames = Directory.GetFiles(ServerFilePath);
-        if (!filenames.Any(f =>
-                f.EndsWith("Wayland.Protocol.Server.csproj") || f.EndsWith("Wayland.Protocol.Client.csproj")))
+        GenerateForClient(protocol);
+        GenerateForServer(protocol);
+    }
+    
+    public static void GenerateForClient(Protocol protocol)
+    {
+        string[] filenames = Directory.GetFiles(ClientFilePath);
+        if (!filenames.Any(f => f.EndsWith("Wayland.Protocol.Client.csproj")))
             throw new Exception("Output directory is not correct. Please set it to the correct path.");
-
-
-        foreach (var filename in filenames)
+        
+        foreach (string filename in filenames)
             if (filename.EndsWith(".gen.cs"))
                 File.Delete(filename);
 
-        foreach (var @interface in protocol.Interfaces)
+        foreach (Interface @interface in protocol.Interfaces)
         {
             if (@interface.Name is "Shm" or "DataOffer")
                 // These are not supported yet due to fd as argument.
                 continue;
 
-            using var fileStream = File.Create(Path.Combine(ServerFilePath, @interface.Name + ".gen.cs"));
+            using FileStream fileStream = File.Create(Path.Combine(ClientFilePath, @interface.Name + ".gen.cs"));
 
-            var source = GenerateSource(@interface);
-            fileStream.Write(source, 0, source.Length);
+            byte[] data = GenerateSource(@interface, false);
+            fileStream.Write(data, 0, data.Length);
         }
     }
 
-    private static byte[] GenerateSource(Interface @interface)
+    public static void GenerateForServer(Protocol protocol)
+    {
+        string[] filenames = Directory.GetFiles(ServerFilePath);
+        if (!filenames.Any(f => f.EndsWith("Wayland.Protocol.Server.csproj")))
+            throw new Exception("Output directory is not correct. Please set it to the correct path.");
+        
+        foreach (string filename in filenames)
+            if (filename.EndsWith(".gen.cs"))
+                File.Delete(filename);
+
+        foreach (Interface @interface in protocol.Interfaces)
+        {
+            if (@interface.Name is "Shm" or "DataOffer")
+                // These are not supported yet due to fd as argument.
+                continue;
+
+            using FileStream fileStream = File.Create(Path.Combine(ServerFilePath, @interface.Name + ".gen.cs"));
+
+            byte[] data = GenerateSource(@interface, true);
+            fileStream.Write(data, 0, data.Length);
+        }
+    }
+
+    private static byte[] GenerateSource(Interface @interface, bool isServer)
     {
         var sb = new StringBuilder();
         sb.Append("namespace Wayland.Protocol.Common;\n");
@@ -53,9 +80,9 @@ public static class CodeGenerator
         sb.Append("    \n");
         AddRequestOpCodeStub(sb, @interface.Requests);
         sb.Append("    \n");
-        AddEventsWrapperStub(sb, @interface.Events);
+        AddEventsWrapperStub(sb, @interface.Events, isServer);
         sb.Append("    \n");
-        AddRequestsWrapperStub(sb, @interface.Requests);
+        AddRequestsWrapperStub(sb, @interface.Requests, isServer);
         sb.Append("}\n");
 
         return Encoding.Default.GetBytes(sb.ToString());
@@ -65,7 +92,8 @@ public static class CodeGenerator
     {
         sb.Append("    private enum EventOpCode : ushort\n");
         sb.Append("    {\n");
-        for (var i = 0; i < events.Count; i++) sb.Append($"        {events[i].Name} = {i},\n");
+        for (int i = 0; i < events.Count; i++)
+            sb.Append($"        {events[i].Name} = {i},\n");
 
         sb.Append("    }\n");
     }
@@ -74,15 +102,54 @@ public static class CodeGenerator
     {
         sb.Append("    private enum RequestOpCode : ushort\n");
         sb.Append("    {\n");
-        for (var i = 0; i < requests.Count; i++) sb.Append($"        {requests[i].Name} = {i},\n");
+        for (int i = 0; i < requests.Count; i++)
+            sb.Append($"        {requests[i].Name} = {i},\n");
 
         sb.Append("    }\n");
     }
 
-    private static void AddEventsWrapperStub(StringBuilder sb, List<Event> events)
+    private static void AddEventsWrapperStub(StringBuilder sb, List<Event> events, bool isServer)
     {
         sb.Append("    public class EventsWrapper(ProtocolObject protocolObject)\n");
         sb.Append("    {\n");
+
+        if (isServer)
+        {
+            AddEventsStub(sb, events);
+        }
+        else
+        {
+            foreach (var request in events)
+            {
+                AddExecutableMethodsStub(sb, request.Name, request.Arguments, false);
+            }
+        }
+
+        sb.Append("    }\n");
+    }
+
+    private static void AddRequestsWrapperStub(StringBuilder sb, List<Request> requests, bool isServer)
+    {
+        sb.Append("    public class RequestsWrapper(ProtocolObject protocolObject)\n");
+        sb.Append("    {\n");
+
+        if (isServer)
+        {
+            foreach (var request in requests)
+            {
+                AddExecutableMethodsStub(sb, request.Name, request.Arguments, true);
+            }
+        }
+        else
+        {
+            AddEventsStub(sb, requests);
+        }
+
+        sb.Append("    }\n");
+    }
+
+    public static void AddEventsStub(StringBuilder sb, List<Event> events)
+    {
         foreach (var @event in events)
             if (@event.Arguments.Count > 0)
                 sb.Append(
@@ -162,26 +229,48 @@ public static class CodeGenerator
             sb.Append("        }\n");
             sb.Append("        \n");
         }
-
-        sb.Append("    }\n");
     }
-
-    private static void AddRequestsWrapperStub(StringBuilder sb, List<Request> requests)
+    
+    public static void AddEventsStub(StringBuilder sb, List<Request> requests)
     {
-        sb.Append("    public class RequestsWrapper(ProtocolObject protocolObject)\n");
-        sb.Append("    {\n");
         foreach (var request in requests)
-        {
             if (request.Arguments.Count > 0)
                 sb.Append(
-                    $"        public void {request.Name}(SocketConnection socketConnection, {string.Join(", ", request.Arguments.Select(arg => arg.Type + " " + arg.Name))})\n");
+                    $"        public Action<{string.Join(", ", request.Arguments.Select(arg => arg.Type))}>? {request.Name} {{ get; set; }}\n");
             else
-                sb.Append($"        public void {request.Name}(SocketConnection socketConnection)\n");
+                sb.Append(
+                    $"        public Action? {request.Name} {{ get; set; }}\n");
 
+        sb.Append("        \n");
+        sb.Append("        internal void HandleEvent(SocketConnection socketConnection)\n");
+        sb.Append("        {\n");
+        sb.Append("            ushort length = socketConnection.ReadUInt16();\n");
+        sb.Append("            ushort opCode = socketConnection.ReadUInt16();\n");
+        sb.Append("            \n");
+        sb.Append("            switch (opCode)\n");
+        sb.Append("            {\n");
+
+        foreach (var request in requests)
+        {
+            sb.Append($"                case (ushort) EventOpCode.{request.Name}:\n");
+            sb.Append($"                    Handle{request.Name}Event(socketConnection, length);\n");
+            sb.Append("                    return;\n");
+        }
+
+        sb.Append("            }\n");
+        sb.Append("        }\n");
+        sb.Append("        \n");
+
+        foreach (var request in requests)
+        {
+            sb.Append(
+                $"        private void Handle{request.Name}Event(SocketConnection socketConnection, ushort length)\n");
             sb.Append("        {\n");
-            sb.Append("            MessageWriter writer = new MessageWriter();\n");
-            sb.Append("            writer.Write(protocolObject.Id);\n");
-            sb.Append($"            writer.Write((int) RequestOpCode.{request.Name});\n");
+            sb.Append("            byte[] buffer = new byte[length / 8];\n");
+            sb.Append("            socketConnection.Read(buffer, 0, buffer.Length);\n");
+            sb.Append('\n');
+            sb.Append("            MessageReader reader = new MessageReader(buffer);\n");
+            sb.Append('\n');
 
             for (var i = 0; i < request.Arguments.Count; ++i)
             {
@@ -189,16 +278,28 @@ public static class CodeGenerator
                 switch (argument.Type)
                 {
                     case "int":
+                        sb.Append($"            int arg{i} = reader.ReadInt();\n");
+                        break;
                     case "uint":
-                    case "string":
-                        sb.Append(
-                            $"            writer.Write({argument.Name});\n");
+                        sb.Append($"            uint arg{i} = reader.ReadUInt();\n");
                         break;
                     case "Fixed":
-                    case "ObjectId":
+                        sb.Append($"            Fixed arg{i} = reader.ReadFixed();\n");
+                        break;
                     case "NewId":
-                        sb.Append(
-                            $"            writer.Write({argument.Name}.Value);\n");
+                        sb.Append($"            NewId arg{i} = reader.ReadNewId();\n");
+                        break;
+                    case "ObjectId":
+                        sb.Append($"            ObjectId arg{i} = reader.ReadObjectId();\n");
+                        break;
+                    case "string":
+                        sb.Append($"            string arg{i} = reader.ReadString();\n");
+                        break;
+                    case "Fd":
+                        sb.Append($"            Fd arg{i} = reader.ReadFd();\n");
+                        break;
+                    case "byte[]":
+                        sb.Append($"            byte[] arg{i} = reader.ReadByteArray();\n");
                         break;
                     default:
                         throw new Exception("Unknown type: " + argument.Type);
@@ -206,16 +307,62 @@ public static class CodeGenerator
             }
 
             sb.Append('\n');
-            sb.Append("            byte[] data = writer.ToArray();\n");
-            sb.Append("            int length = data.Length - 8;\n");
-            sb.Append("            data[5] = (byte)(length >> 8);\n");
-            sb.Append("            data[6] = (byte)(byte.MaxValue << 8 & length);\n");
-            sb.Append('\n');
-            sb.Append("            socketConnection.Write(data);\n");
+            sb.Append(
+                $"            {request.Name}?.Invoke({string.Join(", ", request.Arguments.Select((_, i) => "arg" + i))});\n");
             sb.Append("        }\n");
-            sb.Append('\n');
+            sb.Append("        \n");
+        }
+    }
+
+    private static void AddExecutableMethodsStub(StringBuilder sb, string name, List<Argument> arguments, bool requestWrapper)
+    {
+        if (arguments.Count > 0)
+            sb.Append(
+                $"        public void {name}(SocketConnection socketConnection, {string.Join(", ", arguments.Select(arg => arg.Type + " " + arg.Name))})\n");
+        else
+            sb.Append($"        public void {name}(SocketConnection socketConnection)\n");
+
+        sb.Append("        {\n");
+        sb.Append("            MessageWriter writer = new MessageWriter();\n");
+        sb.Append("            writer.Write(protocolObject.Id);\n");
+        
+        if (requestWrapper)
+            sb.Append($"            writer.Write((int) RequestOpCode.{name});\n");
+        else
+            sb.Append($"            writer.Write((int) EventOpCode.{name});\n");
+
+        for (var i = 0; i < arguments.Count; ++i)
+        {
+            var argument = arguments[i];
+            switch (argument.Type)
+            {
+                case "byte[]":
+                case "int":
+                case "uint":
+                case "string":
+                    sb.Append(
+                        $"            writer.Write({argument.Name});\n");
+                    break;
+                case "Fixed":
+                case "Fd":
+                case "ObjectId":
+                case "NewId":
+                    sb.Append(
+                        $"            writer.Write({argument.Name}.Value);\n");
+                    break;
+                default:
+                    throw new Exception("Unknown type: " + argument.Type);
+            }
         }
 
-        sb.Append("    }\n");
+        sb.Append('\n');
+        sb.Append("            byte[] data = writer.ToArray();\n");
+        sb.Append("            int length = data.Length - 8;\n");
+        sb.Append("            data[5] = (byte)(length >> 8);\n");
+        sb.Append("            data[6] = (byte)(byte.MaxValue << 8 & length);\n");
+        sb.Append('\n');
+        sb.Append("            socketConnection.Write(data);\n");
+        sb.Append("        }\n");
+        sb.Append('\n');
     }
 }
