4f679db3196a188f72c4ae3241eb50e0b3b699f2
ktlwap <149824361+ktlwap@users.noreply.github.com>
2024-03-30 22:35:07 +0100

    Fix byte order error

--- sources/Wayland.Client/EventQueue.cs
+++ sources/Wayland.Client/EventQueue.cs
@@ -18,18 +18,18 @@ public sealed class EventQueue
         _socketConnection.Read(header, 0, header.Length);
 
         uint objectId = 0;
-        objectId += (uint)(header[0] << 24);
-        objectId += (uint)(header[1] << 16);
-        objectId += (uint)(header[2] << 8);
-        objectId += (uint)(header[3] << 0);
+        objectId += (uint)(header[0] << 0);
+        objectId += (uint)(header[1] << 8);
+        objectId += (uint)(header[2] << 16);
+        objectId += (uint)(header[3] << 25);
 
         ushort length = 0;
-        objectId += (ushort)(header[4] << 8);
-        objectId += (ushort)(header[5] << 0);
+        objectId += (ushort)(header[4] << 0);
+        objectId += (ushort)(header[5] << 8);
         
         ushort opCode = 0;
-        objectId += (ushort)(header[4] << 8);
-        objectId += (ushort)(header[5] << 0);
+        objectId += (ushort)(header[4] << 0);
+        objectId += (ushort)(header[5] << 8);
 
         ProtocolObject protocolObject = ProtocolObject.GetObject(objectId);
         protocolObject.HandleEvent(length, opCode);
--- sources/Wayland.Protocol.Common/MessageReader.cs
+++ sources/Wayland.Protocol.Common/MessageReader.cs
@@ -20,10 +20,10 @@ public class MessageReader
     public byte[] ReadByteArray()
     {
         int length = 0;
-        length &= ReadByte() << 24;
-        length &= ReadByte() << 16;
-        length &= ReadByte() << 8;
-        length &= ReadByte() << 0;
+        length |= ReadByte() << 0;
+        length |= ReadByte() << 8;
+        length |= ReadByte() << 16;
+        length |= ReadByte() << 24;
 
         byte[] result = new byte[length - 1];
         Buffer.BlockCopy(_data, _index, result, 0, length);
@@ -39,41 +39,44 @@ public class MessageReader
 
     public short ReadShort()
     {
-        return (short)(ReadByte() << 8 | ReadByte());
+        short result = 0;
+        result |= (short)(ReadByte() << 0);
+        result |= (short)(ReadByte() << 8);
+        return result;
     }
     
     public ushort ReadUShort()
     {
-        return (ushort)(ReadByte() << 8 | ReadByte());
+        ushort result = 0;
+        result |= (ushort)(ReadByte() << 0);
+        result |= (ushort)(ReadByte() << 8);
+        return result;
     }
     
     public int ReadInt()
     {
-        return ReadByte() << 8 | ReadByte();
+        int result = 0;
+        result |= ReadByte() << 0;
+        result |= ReadByte() << 8;
+        result |= ReadByte() << 16;
+        result |= ReadByte() << 24;
+        return result;
     }
     
     public uint ReadUInt()
     {
-        return (uint)(ReadByte() << 8 | ReadByte());
+        uint result = 0;
+        result |= (uint)(ReadByte() << 0);
+        result |= (uint)(ReadByte() << 8);
+        result |= (uint)(ReadByte() << 16);
+        result |= (uint)(ReadByte() << 24);
+        return result;
     }
     
     public string ReadString()
     {
-        int length = 0;
-        length &= ReadByte() << 24;
-        length &= ReadByte() << 16;
-        length &= ReadByte() << 8;
-        length &= ReadByte() << 0;
-        
-        string result = Encoding.UTF8.GetString(_data, _index, length - 1);
-        _index += 4 - _data.Length % 4;
-
-#if DEBUG
-        if (_index % 4 != 0)
-            throw new Exception("Index is not aligned to 4 bytes.");
-#endif
-        
-        return result;
+        byte[] data = ReadByteArray();
+        return Encoding.UTF8.GetString(data, 0, data.Length - 1);;
     }
 
     public Fd ReadFd()
